30ca51bf9801b257f6b4a3b7f6e59dd563e8f2fc
Duc Vu <de.yd315@gmail.com>
2019-04-15 05:27:32 -0500

    included demo for spread and rest operator

--- ES6/others.js
+++ ES6/others.js
@@ -16,6 +16,12 @@ const candies = ["chocolate", "lolipop", "gummy bear"]
 const meals = ["steamed rice", "steak", "brocoli beef", "fried chicken"]
 const fruits = ["bananna", "strawberry", "kiwi", "apple", "orange"]
 
-const food = [...candies, ...meals, ...fruits]
+const food = [...candies, ...meals, ...fruits, 'cake']
 
 console.log(food)
+
+const infoLog = (name, job) => {
+    console.log(`${name}'s occupation is ${job}`)
+}
+
+console.log(infoLog(...['Duc','Programmer']))
