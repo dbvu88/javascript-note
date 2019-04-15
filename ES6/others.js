5fdd48f0026052dc0ae1dfe59baab58ad661b63e
Duc Vu <de.yd315@gmail.com>
2019-04-15 05:22:19 -0500

    included demo for spread and rest operator

--- ES6/others.js
+++ ES6/others.js
@@ -11,3 +11,11 @@ const myFuction = (mainParam, ...rest) => {
 myFuction("primary", 1, 2, 3, 4, 5, 'secondary', 'addition', 'blah blah blah');
 
 // spread paramters
+
+const candies = ["chocolate", "lolipop", "gummy bear"]
+const meals = ["steamed rice", "steak", "brocoli beef", "fried chicken"]
+const fruits = ["bananna", "strawberry", "kiwi", "apple", "orange"]
+
+const food = [...candies, ...meals, ...fruits]
+
+console.log(food)
